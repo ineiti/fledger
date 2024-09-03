0ccfd7fda3790a65bf6f29ae22d75d36bb57a8b7
Linus Gasser <linus.gasser@epfl.ch>
2024-09-03 15:04:30 +0100

    Simplifying landing page, including bootstrap.js

--- flbrowser/src/main.rs
+++ flbrowser/src/main.rs
@@ -63,6 +63,7 @@ fn main() {
         let your_message: HtmlTextAreaElement = web.get_element("your_message");
         let proxy_div: HtmlDivElement = web.get_element("proxy_div");
         let proxy_url: HtmlInputElement = web.get_element("proxy_url");
+        let webproxy = web.node.webproxy.as_mut().unwrap().clone();
 
         loop {
             if let Ok(btn) = rx.try_recv() {
@@ -83,22 +84,23 @@ fn main() {
                         downloadFile("gossip_event.toml".into(), data.into());
                     }
                     Button::WebProxy => {
-                        let mut response = web
-                            .node
-                            .webproxy
-                            .as_mut()
-                            .unwrap()
-                            .get(&proxy_url.value())
-                            .await
-                            .expect("Getting response");
-                        let proxy = proxy_div.clone();
+                        let proxy_div = proxy_div.clone();
+                        let proxy_url = proxy_url.clone();
+                        let mut webproxy = webproxy.clone();
                         spawn_local(async move {
+                            let fetching =
+                                format!("Fetching url from proxy: {}", proxy_url.value());
+                            proxy_div.set_inner_html(&fetching);
+                            let mut response = webproxy
+                                .get(&proxy_url.value())
+                                .await
+                                .expect("Getting response");
                             let text = format!(
                                 "Proxy: {}<br>{}",
                                 response.proxy(),
                                 response.text().await.unwrap()
                             );
-                            proxy.set_inner_html(&text);
+                            proxy_div.set_inner_html(&text);
                         });
                     }
                 }
@@ -110,7 +112,6 @@ fn main() {
                 web.set_html_id("node_info", state.get_node_name());
                 web.set_html_id("version", state.get_version());
                 web.set_html_id("messages", state.get_msgs());
-                web.set_html_id("nodes_known", format!("{}", state.nodes_known));
                 web.set_html_id("nodes_online", format!("{}", state.nodes_online));
                 web.set_html_id("nodes_connected", format!("{}", state.nodes_connected));
             }
@@ -148,7 +149,7 @@ impl FledgerWeb {
         FledgerWeb::set_data_storage();
 
         wasm_logger::init(wasm_logger::Config::new(log::Level::Debug));
-        log::info!("Starting new FledgerWeb");
+        log::info!("Starting new FledgerWeb on {URL}");
 
         // Get a link to the document
         let window = web_sys::window().expect("no global `window` exists");
@@ -252,7 +253,6 @@ pub struct FledgerState {
     pub msgs_system: u32,
     pub msgs_local: u32,
     pub mana: u32,
-    pub nodes_known: usize,
     pub nodes_online: usize,
     pub nodes_connected: usize,
 }
@@ -291,7 +291,6 @@ impl FledgerState {
         let nodes_info = node.nodes_info_all()?;
         Ok(Self {
             info,
-            nodes_known: nodes_info.len(),
             nodes_online: node.nodes_online()?.len(),
             nodes_connected: node.nodes_connected()?.len(),
             msgs: FledgerMessages::new(msgs, &nodes_info.clone().into_values().collect()),
