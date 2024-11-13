7b3ec92b94a8782f0d02cb4f83e7c352071ffd15
Linus Gasser <linus.gasser@epfl.ch>
2024-11-13 13:39:11 +0100

    Adding node names to simulation output

--- start_simul.sh
+++ start_simul.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/usr/bin/env bash -e
 
 make kill
 
@@ -16,15 +16,15 @@ SIMUL=simul/
 rm -rf $SIMUL
 mkdir -p $SIMUL
 
-./target-common/release/flsignal -vv &
+./target-common/release/flsignal -vv |& ts "Signal " &
 
 for NODE in $( seq $NODES ); do
-  NAME="node_$(printf "%02d" $NODE)"
+  NAME="NODE_$(printf "%02d" $NODE)"
   echo "Starting node $NAME"
   CONFIG="$SIMUL$NAME/"
   PATH_LEN_ARG=""
   if [ $NODE = "1" ]; then
     PATH_LEN_ARG="--path-len $PATH_LEN"
   fi
-  ./target-common/release/fledger --config $CONFIG --name $NAME -vv -s ws://localhost:8765 $PATH_LEN_ARG &
+  ./target-common/release/fledger --config $CONFIG --name $NAME -vv -s ws://localhost:8765 $PATH_LEN_ARG |& ts "$NAME" &
 done
