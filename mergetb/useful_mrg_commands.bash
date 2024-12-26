638fe061321828ee60d90156922924c3f24b0858
Derya <deryacog@gmail.com>
2024-12-26 13:28:28 +0100

    cleanup and reorganize

--- mergetb/useful_mrg_commands.bash
+++ mergetb/useful_mrg_commands.bash
@@ -1,23 +1,26 @@
 # mrg xdc scp upload -x fledgerxdc.dcog ./target-common/release/fledger node-2:/home/dcog/fledger
 
-# ansible -i inventory.ini ALL_NODES -m ping
 
-# mrg xdc ssh -x fledgerxdc.dcog node-1
+# ssh into mergetb 
+mrg xdc ssh fledgerxdc.dcog 
+mrg xdc ssh -x fledgerxdc.dcog node-1
 
+# ping all nodes
+ansible -i inventory.ini ALL_NODES -m ping
 
 # generate inventory and send it to xdc
 mrg show materialization tryagain.fledgerfirst.dcog > materialization.txt
-# mrg show materialization twonode.fledgerfirst.dcog > materialization_twonode.txt
 python3 generate_inventory.py
-mrg xdc scp upload inventory.ini fledgerxdc.dcog:/home/dcog
-mrg xdc scp upload inventory_copy.ini fledgerxdc.dcog:/home/dcog
-mrg xdc scp upload playbook.yml fledgerxdc.dcog:/home/dcog
-mrg xdc scp upload playbook_copy.yml fledgerxdc.dcog:/home/dcog
-mrg xdc scp upload install_docker.yml fledgerxdc.dcog:/home/dcog
-mrg xdc scp upload delete_docker.yml fledgerxdc.dcog:/home/dcog
 
-nohup ansible-playbook -i inventory.ini delete_docker.yml > playbook.log 2>&1
-nohup ./get_simul_data.sh > logs.txt 2>&1 &
+# upload files to xdc
+mrg xdc scp upload inventory.ini fledgerxdc2.dcog:/home/dcog
+mrg xdc scp upload playbook.yml fledgerxdc2.dcog:/home/dcog
+mrg xdc scp upload install_docker.yml fledgerxdc2.dcog:/home/dcog
+mrg xdc scp upload delete_docker.yml fledgerxdc2.dcog:/home/dcog
+mrg xdc scp upload stop_containers.yml fledgerxdc2.dcog:/home/dcog
+mrg xdc scp upload ../../loopix_core_config.yaml fledgerxdc2.dcog:/home/dcog
+
+
 
 # # build and upload binaries
 # (cd ../cli/fledger && cargo build -r)
@@ -25,11 +28,13 @@ nohup ./get_simul_data.sh > logs.txt 2>&1 &
 # mrg xdc scp upload ../target-common/release/flsignal fledgerxdc.dcog:/home/dcog
 # mrg xdc scp upload ../target-common/release/fledger fledgerxdc.dcog:/home/dcog
 
-
+# run experiment
+nohup ansible-playbook -i inventory.ini delete_docker.yml > playbook.log 2>&1
 nohup ./get_simul_data.sh > logs.txt 2>&1 &
 nohup ansible-playbook -i inventory.ini playbook.yml --extra-vars "retry=0 path_len=3 variable=lambda_payload index=0" > logs.txt 2>&1 > logs.txt 2>&1 &
 
-mrg xdc ssh fledgerxdc.dcog 
+
+
 
 
 
