3256f105aeeb16f55b66650e6cddc893ccb76875
Sōsuke Aizen <yohan@abehssera.com>
2025-03-24 01:42:20 +0100

    feat(experiment): exp2-dummy10

---
+++ experiments/exp2-dummy10/model.py
@@ -0,0 +1,17 @@
+from mergexp import *
+
+net = Network('exp2')
+
+def makeNode(i: int):
+    name = chr(ord('a') + i)
+    return net.node(name, proc.cores>=1, memory.capacity>=mb(512))
+
+sna = [makeNode(i) for i in range(10)]
+
+link = net.connect(sna)
+
+for i in range(10):
+    suffix = str(i + 1)
+    link[sna[i]].socket.addrs = ip4(f"10.0.0.{suffix}/24")
+
+experiment(net)
